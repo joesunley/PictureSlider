27e92702843e7554286692c2c02b9f9ab5cc1e03
Joe Sunley <joseph@jossy.co.uk>
2021-07-28 12:51:08 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,224 @@
+﻿
+namespace PictureSlider
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pbBottomRight = new System.Windows.Forms.PictureBox();
+            this.pbBottomCentre = new System.Windows.Forms.PictureBox();
+            this.pbBottomLeft = new System.Windows.Forms.PictureBox();
+            this.pbCentreRight = new System.Windows.Forms.PictureBox();
+            this.pbCentreCentre = new System.Windows.Forms.PictureBox();
+            this.pbCentreLeft = new System.Windows.Forms.PictureBox();
+            this.pbTopRight = new System.Windows.Forms.PictureBox();
+            this.pbTopCentre = new System.Windows.Forms.PictureBox();
+            this.pbTopLeft = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomCentre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreCentre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopRight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopCentre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopLeft)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pbBottomRight);
+            this.panel1.Controls.Add(this.pbBottomCentre);
+            this.panel1.Controls.Add(this.pbBottomLeft);
+            this.panel1.Controls.Add(this.pbCentreRight);
+            this.panel1.Controls.Add(this.pbCentreCentre);
+            this.panel1.Controls.Add(this.pbCentreLeft);
+            this.panel1.Controls.Add(this.pbTopRight);
+            this.panel1.Controls.Add(this.pbTopCentre);
+            this.panel1.Controls.Add(this.pbTopLeft);
+            this.panel1.Location = new System.Drawing.Point(63, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 0;
+            // 
+            // pbBottomRight
+            // 
+            this.pbBottomRight.Location = new System.Drawing.Point(200, 200);
+            this.pbBottomRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pbBottomRight.Name = "pbBottomRight";
+            this.pbBottomRight.Size = new System.Drawing.Size(100, 100);
+            this.pbBottomRight.TabIndex = 8;
+            this.pbBottomRight.TabStop = false;
+            this.pbBottomRight.Tag = "8";
+            this.pbBottomRight.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbBottomCentre
+            // 
+            this.pbBottomCentre.Location = new System.Drawing.Point(100, 200);
+            this.pbBottomCentre.Margin = new System.Windows.Forms.Padding(0);
+            this.pbBottomCentre.Name = "pbBottomCentre";
+            this.pbBottomCentre.Size = new System.Drawing.Size(100, 100);
+            this.pbBottomCentre.TabIndex = 7;
+            this.pbBottomCentre.TabStop = false;
+            this.pbBottomCentre.Tag = "7";
+            this.pbBottomCentre.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbBottomLeft
+            // 
+            this.pbBottomLeft.Location = new System.Drawing.Point(0, 200);
+            this.pbBottomLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pbBottomLeft.Name = "pbBottomLeft";
+            this.pbBottomLeft.Size = new System.Drawing.Size(100, 100);
+            this.pbBottomLeft.TabIndex = 6;
+            this.pbBottomLeft.TabStop = false;
+            this.pbBottomLeft.Tag = "6";
+            this.pbBottomLeft.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbCentreRight
+            // 
+            this.pbCentreRight.Location = new System.Drawing.Point(200, 100);
+            this.pbCentreRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pbCentreRight.Name = "pbCentreRight";
+            this.pbCentreRight.Size = new System.Drawing.Size(100, 100);
+            this.pbCentreRight.TabIndex = 5;
+            this.pbCentreRight.TabStop = false;
+            this.pbCentreRight.Tag = "5";
+            this.pbCentreRight.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbCentreCentre
+            // 
+            this.pbCentreCentre.Location = new System.Drawing.Point(100, 100);
+            this.pbCentreCentre.Margin = new System.Windows.Forms.Padding(0);
+            this.pbCentreCentre.Name = "pbCentreCentre";
+            this.pbCentreCentre.Size = new System.Drawing.Size(100, 100);
+            this.pbCentreCentre.TabIndex = 4;
+            this.pbCentreCentre.TabStop = false;
+            this.pbCentreCentre.Tag = "4";
+            this.pbCentreCentre.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbCentreLeft
+            // 
+            this.pbCentreLeft.Location = new System.Drawing.Point(0, 100);
+            this.pbCentreLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pbCentreLeft.Name = "pbCentreLeft";
+            this.pbCentreLeft.Size = new System.Drawing.Size(100, 100);
+            this.pbCentreLeft.TabIndex = 3;
+            this.pbCentreLeft.TabStop = false;
+            this.pbCentreLeft.Tag = "3";
+            this.pbCentreLeft.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbTopRight
+            // 
+            this.pbTopRight.Location = new System.Drawing.Point(200, 0);
+            this.pbTopRight.Margin = new System.Windows.Forms.Padding(0);
+            this.pbTopRight.Name = "pbTopRight";
+            this.pbTopRight.Size = new System.Drawing.Size(100, 100);
+            this.pbTopRight.TabIndex = 2;
+            this.pbTopRight.TabStop = false;
+            this.pbTopRight.Tag = "2";
+            this.pbTopRight.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbTopCentre
+            // 
+            this.pbTopCentre.Location = new System.Drawing.Point(100, 0);
+            this.pbTopCentre.Margin = new System.Windows.Forms.Padding(0);
+            this.pbTopCentre.Name = "pbTopCentre";
+            this.pbTopCentre.Size = new System.Drawing.Size(100, 100);
+            this.pbTopCentre.TabIndex = 1;
+            this.pbTopCentre.TabStop = false;
+            this.pbTopCentre.Tag = "1";
+            this.pbTopCentre.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // pbTopLeft
+            // 
+            this.pbTopLeft.Location = new System.Drawing.Point(0, 0);
+            this.pbTopLeft.Margin = new System.Windows.Forms.Padding(0);
+            this.pbTopLeft.Name = "pbTopLeft";
+            this.pbTopLeft.Size = new System.Drawing.Size(100, 100);
+            this.pbTopLeft.TabIndex = 0;
+            this.pbTopLeft.TabStop = false;
+            this.pbTopLeft.Tag = "0";
+            this.pbTopLeft.Click += new System.EventHandler(this.Tile_Clicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(138, 367);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Choose New Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Open_Clicked);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 421);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomCentre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBottomLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreCentre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbCentreLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopRight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopCentre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbTopLeft)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pbBottomRight;
+        private System.Windows.Forms.PictureBox pbBottomCentre;
+        private System.Windows.Forms.PictureBox pbBottomLeft;
+        private System.Windows.Forms.PictureBox pbCentreRight;
+        private System.Windows.Forms.PictureBox pbCentreCentre;
+        private System.Windows.Forms.PictureBox pbCentreLeft;
+        private System.Windows.Forms.PictureBox pbTopRight;
+        private System.Windows.Forms.PictureBox pbTopCentre;
+        private System.Windows.Forms.PictureBox pbTopLeft;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using PictureSlider.Properties;
+
+namespace PictureSlider
+{
+     
+    public partial class Form1 : Form
+    {
+        bool[,] grid = new bool[3, 3];
+        List<PictureBox> pBoxes = new List<PictureBox>(9);
+        Random rnd = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitPBoxes();
+            InitImg(Resources.InitialImage);
+        }
+
+        void InitPBoxes()
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                foreach (PictureBox p in panel1.Controls)
+                {
+                    if (Convert.ToInt32(p.Tag) == i)
+                        pBoxes.Add(p);
+                }
+            }
+        }
+
+        void InitImg(string fileLoc)
+        {
+            Image[] imgArr = new Image[9];
+            Image img = ResizeImage(Image.FromFile(fileLoc), 300, 300);
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int index = i * 3 + j;
+                    imgArr[index] = new Bitmap(100, 100);
+                    Graphics g = Graphics.FromImage(imgArr[index]);
+                    g.DrawImage(img, new Rectangle(0, 0, 100, 100), new Rectangle(i * 100, j * 100, 100, 100), GraphicsUnit.Pixel);
+                    g.Dispose();
+                }
+            }
+
+            List<Image> images = imgArr.ToList();
+            for (int i = 0; i < 9; i++)
+            {
+                int r = rnd.Next(0, images.Count);
+                pBoxes[i].Image = images[r];
+                images.RemoveAt(r);
+            }
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    grid[i, j] = true;
+
+            int n = rnd.Next(0, 8);
+            int[] loc = FromIndex(n);
+
+            pBoxes[n].Image = null;
+            pBoxes[n].BackColor = Color.White;
+            grid[loc[0], loc[1]] = false;
+
+        }
+
+        void InitImg(Image image)
+        {
+            Image[] imgArr = new Image[9];
+            Image img = ResizeImage(image, 300, 300);
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    int index = i * 3 + j;
+                    imgArr[index] = new Bitmap(100, 100);
+                    Graphics g = Graphics.FromImage(imgArr[index]);
+                    g.DrawImage(img, new Rectangle(0, 0, 100, 100), new Rectangle(i * 100, j * 100, 100, 100), GraphicsUnit.Pixel);
+                    g.Dispose();
+                }
+            }
+
+            List<Image> images = imgArr.ToList();
+            for (int i = 0; i < 9; i++)
+            {
+                int r = rnd.Next(0, images.Count);
+                pBoxes[i].Image = images[r];
+                images.RemoveAt(r);
+            }
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    grid[i, j] = true;
+
+            int n = rnd.Next(0, 8);
+            int[] loc = FromIndex(n);
+
+            pBoxes[n].Image = null;
+            pBoxes[n].BackColor = Color.White;
+            grid[loc[0], loc[1]] = false;
+        }
+
+        Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+
+        int[] FromIndex(int i)
+        {
+            if (i == 0)
+                return new int[] { 0, 0 };
+
+            int rem;
+            int div = Math.DivRem(i, 3, out rem);
+            return new int[] { rem, div };
+        }
+        int AsIndex(int[] i)
+        {
+            int div = i[1],
+                rem = i[0];
+
+            return Convert.ToInt32(3f * (div + ((float)rem / 3)));
+        }
+
+        int CheckSwap(int i)
+        {
+            int[] loc = FromIndex(i);
+            int x = loc[0],
+                y = loc[1];
+
+            try
+            {
+                if (x + 1 < 3)
+                    if (grid[x + 1, y] == false)
+                        return AsIndex(new int[] { x + 1, y });
+                if (x - 1 > -1)
+                    if (grid[x - 1, y] == false)
+                        return AsIndex(new int[] { x - 1, y });
+                if (y + 1 < 3)
+                    if (grid[x, y + 1] == false)
+                        return AsIndex(new int[] { x, y + 1 });
+                if (y - 1 > -1)
+                    if (grid[x, y - 1] == false)
+                        return AsIndex(new int[] { x, y - 1 });
+                return -1;
+            }
+            catch { return -1; }
+        }
+
+        void DoSwap(int a, int b)
+        {
+            PictureBox first = new PictureBox(),
+                second = new PictureBox();
+            foreach (PictureBox p in panel1.Controls)
+            {
+                if (Convert.ToInt32(p.Tag) == a)
+                    first = p;
+                if (Convert.ToInt32(p.Tag) == b)
+                    second = p;
+            }
+
+            Image tmpImg = first.Image;
+            first.Image = second.Image;
+            second.Image = tmpImg;
+
+            int[] a2 = FromIndex(a);
+            int[] b2 = FromIndex(b);
+
+            grid[a2[0], a2[1]] = false;
+            grid[b2[0], b2[1]] = true;
+        }
+
+        void Tile_Clicked(object sender, EventArgs e)
+        {
+            PictureBox p = (PictureBox)sender;
+            int a = Convert.ToInt32(p.Tag);
+            int b = CheckSwap(a);
+
+            if (b != -1)
+                DoSwap(a, b);
+        }
+
+        void Open_Clicked(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Image files|*.bmp;*.jpg;*.gif;*.png;*.tif|Bitmaps|*.bmp|PNG files|*.png|JPEG files|*.jpg|GIF files|*.gif|TIFF files|*.tif|All files|*.*";
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.ShowDialog();
+
+            string fileLoc = openFileDialog1.FileName;
+
+            if (File.Exists(fileLoc))
+                InitImg(fileLoc);
+        }
+    }
+}
